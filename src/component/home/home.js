75fd14dd9ccb9fa437ec74222747d7de2570915c
sujkh <sujkh@nomadconnection.com>
2017-03-08 11:16:04 +0900

    route test

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default class Home extends React.Component {
   render() {
+      console.log('this.props',this.props)
     return (
       <div>
-        <h1>It Works!!!!!!</h1>
+        <h1 onClick={()=>{
+                this.props.push('/mimi')
+            }}>It Works</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
       </div>
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import MainAppContainer from './containers/MainAppContainer';
 import Home from './component/home/home';
+import Test from './component/test';
 
 export default (
     <Route path="/" component={MainAppContainer}>
         <IndexRoute component={Home}/>
+        <Route path='/mimi' component={Test}/>
     </Route>
 );
